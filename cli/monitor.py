295432d47c77d70ef4279bd7c2f00ca2d40e2557
dayat81 <hiidaayaat@gmail.com>
2024-09-06 16:01:25 +0700

    tested, more stable

--- cli/monitor.py
+++ cli/monitor.py
@@ -1,5 +1,12 @@
+import argparse
 import subprocess
 import time
+
+from prometheus_client import start_http_server, Gauge
+label_names = ['ip']
+throughput_allowed = Gauge('throughput_allowed', 'Throughput',labelnames=label_names)
+throughput_blocked = Gauge('throughput_blocked', 'Throughput',labelnames=label_names)
+
 
 def run_counter_cli(traffic_type):
     result = subprocess.run(['./counter_cli', traffic_type], capture_output=True, text=True)
@@ -16,12 +23,13 @@ def calculate_delta(previous, current):
     delta = {}
     for ip, count in current.items():
         if ip in previous:
-            delta[ip] = count - previous[ip]
+            delta[ip] = max(0, count - previous[ip])
         else:
             delta[ip] = count
     return delta
 
-def monitor_traffic():
+def monitor_traffic(interval):
+    start_http_server(8000)
     previous_allowed = None
     previous_blocked = None
     while True:
@@ -34,17 +42,23 @@ def monitor_traffic():
             
             print("Delta values for allowed traffic:")
             for ip, count in delta_allowed.items():
-                print(f"{ip}: {count}")
+                #print(f"{ip}: {count}")
+                throughput_allowed.labels(ip=ip).set(count/interval)
             
-            print("\nDelta values for blocked traffic:")
+            #print("\nDelta values for blocked traffic:")
             for ip, count in delta_blocked.items():
-                print(f"{ip}: {count}")
+                #print(f"{ip}: {count}")
+                throughput_blocked.labels(ip=ip).set(count/interval)
         else:
             print("Initial data collected. Delta will be available on the next run.")
         
         previous_allowed = current_allowed
         previous_blocked = current_blocked
-        time.sleep(5)
+        time.sleep(interval)
 
 if __name__ == "__main__":
-    monitor_traffic()
+    parser = argparse.ArgumentParser(description="Monitor network traffic")
+    parser.add_argument("--interval", type=int, default=5, help="Interval between checks in seconds")
+    args = parser.parse_args()
+    
+    monitor_traffic(args.interval)
